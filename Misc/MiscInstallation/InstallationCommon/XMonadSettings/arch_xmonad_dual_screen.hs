535b9cf99c18fb93082ff887ba5c54562331dd06
daniel_li <daniel_li@tw.trendnet.org>
2013-02-06 13:30:25 +0800

    add new layout of xmonad

--- Misc/MiscInstallation/InstallationCommon/XMonadSettings/arch_xmonad_dual_screen.hs
+++ Misc/MiscInstallation/InstallationCommon/XMonadSettings/arch_xmonad_dual_screen.hs
@@ -1,29 +1,31 @@
 import System.IO
 import System.IO.UTF8
 import System.Exit
+import Data.Ratio
+import qualified Data.Map        as M
+import qualified XMonad.StackSet as W
 import XMonad
+import XMonad.Util.Run
+import XMonad.Util.EZConfig
+import XMonad.Hooks.SetWMName
+import XMonad.Hooks.ICCCMFocus
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
-import XMonad.Hooks.ManageHelpers
-import XMonad.Hooks.SetWMName
-import XMonad.Layout.NoBorders
-import XMonad.Layout.Spiral
-import XMonad.Layout.Tabbed
-import XMonad.Layout.Combo
-import XMonad.Layout.Decoration
-import XMonad.Layout.Maximize
-import XMonad.Layout.ResizableTile
-import XMonad.Layout.SimplestFloat
-import XMonad.Layout.TwoPane
-import XMonad.Layout.WindowNavigation
-import XMonad.Util.Run
 import XMonad.Hooks.EwmhDesktops
-import XMonad.Util.EZConfig
-import XMonad.Hooks.ICCCMFocus
+import XMonad.Hooks.ManageHelpers
+import XMonad.Layout.Grid
+import XMonad.Layout.Combo
+import XMonad.Layout.Tabbed
+import XMonad.Layout.Spiral
+import XMonad.Layout.TwoPane
+import XMonad.Layout.Maximize
+import XMonad.Layout.NoBorders
+import XMonad.Layout.Magnifier
+import XMonad.Layout.Decoration
+import XMonad.Layout.SimplestFloat
+import XMonad.Layout.ResizableTile
+import XMonad.Layout.WindowNavigation
 import XMonad.Layout.IndependentScreens
-import qualified XMonad.StackSet as W
-import qualified Data.Map        as M
-
 
 ------------------------------------------------------------------------
 -- Terminal
@@ -75,20 +77,20 @@ myManageHook = composeAll
 -- The available layouts.  Note that each layout is separated by |||,
 -- which denotes layout choice.
 --
-myLayout = avoidStruts (Full ||| tiled' ||| myTabbed ||| combo ||| simplestFloat) ||| Full
-           where
-             -- The default number of windows in the master pane
-             nmaster = 1
-             -- Default proportion of screen occupied by master pane
-             ratio = 1/2
-             -- Percent of screen to increment by when resizing panes
-             delta = 3/100
-             -- Tabbed Layout
-             myTabbed = tabbed shrinkText myTabTheme
-             -- Shorthand for ResizableTall
-             tiled' = maximize $ ResizableTall nmaster delta ratio []
-             -- Combined layout
-             combo = windowNavigation ( combineTwo (TwoPane delta ratio) (myTabbed) (tiled'))
+myLayout = avoidStruts (tiled ||| Mirror tiled  ||| magnify Grid ||| Full ||| tabs) ||| Full
+  where
+     -- default tiling algorithm partitions the screen into two panes
+     tiled   = ResizableTall nmaster delta ratio []
+     -- The default number of windows in the master pane
+     nmaster = 1
+     -- Default proportion of screen occupied by master pane
+     ratio   = 8 % 13
+     -- Percent of screen to increment by when resizing panes
+     delta   = 3 % 100
+     -- tabbed layout
+     tabs = tabbed shrinkText myTabTheme
+     -- magnification in grid
+     magnify = magnifiercz (14 % 10)
 
 ------------------------------------------------------------------------
 -- Colors and borders
